d8291e11858554b194db217727f40167406ba50f
Davide Ferrari <davideferrari8@gmail.com>
2012-07-09 10:18:34 +0200

    Introduzione dell'header errori.h In questo header inseriamo tutti i valori di ritorno del programma quando si incorre in un qualsiasi malfunzionamento. La descrizione dell'errore indicherà qual è stato il problema.

---
+++ errori.h
@@ -0,0 +1,10 @@
+#ifndef ERRORI_H
+#define ERRORI_H
+
+/** Header degli errori
+  * Qui sono elencati tutti gli errori di rotorno del programma e la relativa
+  * descrizione.
+  */
+
+
+#endif // ERRORI_H
