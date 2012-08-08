60ef9928a9533498416cd5d640cf8d01b1a2a741
Serena Ziviani <senseriumi@gmail.com>
2012-08-08 18:08:56 +0200

    finalmente matrici rettangolari! =)

--- main.h
+++ main.h
@@ -28,7 +28,7 @@ using namespace std;
   * 14  GD1         GD2     GD3
   * 15  TRACE       GD1     GD2     GD3
   */
-#define MASK 2
+#define MASK 15
 
 #define DBG(a, b)      {if ((a) & MASK) {b; }}
 #else
