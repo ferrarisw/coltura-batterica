3a2b061fc6546f77752f7dec33bd28363728dffd
Serena Ziviani <senseriumi@gmail.com>
2012-09-18 23:34:00 +0200

    aggiunta documentazione

--- closingalert.cpp
+++ closingalert.cpp
@@ -15,3 +15,4 @@ ClosingAlert::ClosingAlert(QWidget * parent):
     setLayout(alertlayout);
 
 }
+
--- closingalert.h
+++ closingalert.h
@@ -3,13 +3,20 @@
 
 #include <QtGui>
 
-
+/**
+ * Widget di conferma della chiusura del programma.
+ *  Membri pubblici:
+ *      -Il costruttore
+ *      -Il QDialogButtonBox buttons, che definisce i bottoni "ok" e "annulla".
+ */
 class ClosingAlert : public QWidget
 {
     Q_OBJECT
 public:
     ClosingAlert(QWidget *parent = 0);
     QDialogButtonBox * buttons;
+
+
 };
 
 #endif // CLOSINGALERT_H
